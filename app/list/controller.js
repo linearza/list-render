cee7c4a0816792b98a9fafcf8247aad42593b780
Donovan Graham <donovan@platform7.com>
2015-09-10 16:39:49 +0200

    Push pop shift and unshift

--- app/list/controller.js
+++ app/list/controller.js
@@ -3,13 +3,46 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   items: [],
+  nextId: 1,
   isActive: false,
 
   actions: {
     fix: null,
 
-    doToggle() {
+    toggle() {
       this.toggleProperty('isActive');
-    }
+    },
+
+    push() {
+      let item = Ember.Object.create({
+        id: this.get('nextId'),
+        title: "Hey " + this.get('nextId'),
+        status: Math.round(Math.random())
+      });
+
+      this.incrementProperty('nextId');
+      this.get('items').pushObject(item);
+    },
+
+    pop() {
+      this.get('items').popObject();
+    },
+
+
+    unshift() {
+      let item = Ember.Object.create({
+        id: this.get('nextId'),
+        title: "Hey " + this.get('nextId'),
+        status: Math.round(Math.random())
+      });
+
+      this.incrementProperty('nextId');
+      this.get('items').unshiftObject(item);
+    },
+
+    shift() {
+      this.get('items').shiftObject();
+    },
+
   }
 });
